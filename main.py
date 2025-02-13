1590ebecd7d84d172fbd9a59b000bcd1c87a8cc0
Mikhail Khaykin <m.khaykin@outlook.com>
2025-02-13 13:56:18 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    main()
