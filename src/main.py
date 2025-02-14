c5f91d596707197d842f77d43bf2db2450e44faa
mikhail.khaykin <m.khaykin@outlook.com>
2025-02-14 13:08:56 +0300

    feat: tk + select file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
 import logging
+import os
+import sys
+import tkinter as tk
+from tkinter import filedialog
 
 from update_fields import update_fields
 
 logger = logging.getLogger(__name__)
 
+FILENAME = (
+    "..\\samples\\input.docx" if sys.platform == "win32" else "../samples/input.docx"
+)
+
+
+def _select_file() -> str:
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    return file_path
+
 
 def main() -> None:
-    update_fields("samples/input.docx")
+    source_file: str = os.path.abspath(FILENAME)
+
+    if not os.path.exists(source_file):
+        source_file = _select_file()
+
+    update_fields(source_file)
 
 
 if __name__ == "__main__":
